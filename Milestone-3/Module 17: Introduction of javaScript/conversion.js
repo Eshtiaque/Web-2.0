912e1447d7a896e8773060b24683897d150ba006
Eshtiaque <est.ahmed111@gmail.com>
2023-02-02 03:45:27 +0600

    llll

---
+++ Milestone-3/Module 17: Introduction of javaScript/conversion.js
@@ -0,0 +1,8 @@
+var price1 = 30 ;
+var price2 = 20 ;
+var totalPrice =price1 +price2;
+// console.log(totalPrice);
+var firstName ="jennie";
+var lastName ="lim";
+var fullName = firstName + ' ' + lastName;
+console.log(fullName)
