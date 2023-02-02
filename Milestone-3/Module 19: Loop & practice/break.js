e82c8964a305f1a46d82f9a0161c082571feed4a
Eshtiaque <est.ahmed111@gmail.com>
2023-02-03 02:18:53 +0600

    L

---
+++ Milestone-3/Module 19: Loop & practice/break.js
@@ -0,0 +1,18 @@
+for(var i=0;i<20;i++){
+    // var number=numbers[i];
+    console.log(i);
+    if(i>10){
+        break;
+    }
+}
+console.log('----------------------------');
+
+var rostGiven = 0;
+while (rostGiven<10){
+    console.log('rost den please');
+    // console.log(rostGiven);
+    rostGiven ++;
+    if(rostGiven>4){
+        break;
+    }
+}
