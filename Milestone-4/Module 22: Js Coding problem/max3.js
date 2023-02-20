8fb5a8083fcb3a5d53f5380b55a84954bbc1d081
Eshtiaque <est.ahmed111@gmail.com>
2023-02-20 13:56:46 +0600

    nL

---
+++ Milestone-4/Module 22: Js Coding problem/max3.js
@@ -0,0 +1,12 @@
+const jim =84;
+const dela = 99;
+const cinku = 77
+if(jim>dela && jim>cinku){
+    console.log('jim will get the cake ');
+}
+else if(dela>jim && dela>cinku){
+        console.log('Dela will get the cake ');
+    }
+else{
+    console.log('cinku will get the cake')
+}
