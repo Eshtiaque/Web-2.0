1c8a427aea31b43f9ca14bd130e1fe49d0d29517
Eshtiaque <est.ahmed111@gmail.com>
2023-02-27 21:58:16 +0600

    L

--- Milestone-6/Module 34: API & edge/js/countries.js
+++ Milestone-6/Module 34: API & edge/js/countries.js
@@ -9,14 +9,33 @@ const displayCountries = countries =>{
     //     console.log(country);
     // }
     countries.forEach(country=>{
-        console.log(country);
+        // console.log(country.cca2);
         const countryDiv =document.createElement('div');
     countryDiv.classList.add('country')
     countryDiv.innerHTML =`
     <h3>Name:${country.name.common}</h3>
     <p>capital:${country.capital?country.capital[0]:'No capital'}</p>
+    <button onclick="loadCountryCode ('${country.cca2}')">Click</button>
     `;
     countriesContainer.appendChild(countryDiv);
     })
 }
+const loadCountryCode =code =>{
+    //https://restcountries.com/v3.1/alpha/{code}
+    const url =`https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('details' ,code);
+    // console.log(url);
+    fetch(url)
+    .then (res=>res.json())
+ .then(data => showCountryDetails(data[0]));
+} 
+const showCountryDetails =country=>{
+    console.log(country);
+    const detailsContainer = document.getElementById('country-details')
+    detailsContainer.innerHTML =`
+    <h3>Name:${country.name.common} </h3>
+    <img src ="${country.flags.png}">
+    `
+}
+
 loadCountries();
