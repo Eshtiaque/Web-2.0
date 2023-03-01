15b4327318f22118239c0f165a8cdfe6a7b92d19
Eshtiaque <est.ahmed111@gmail.com>
2023-03-01 17:25:16 +0600

    L

--- Milestone-6/Module 34: API & edge/js/course.js
+++ Milestone-6/Module 34: API & edge/js/course.js
@@ -4,7 +4,18 @@ const loadQuote =()=>{
  .then(data => displayQuote(data))
 // console.log('quotes loading');
 }
+const loadQuote2 =async()=>{
+    try{
+        const res = await fetch('https://api.kanye.rest/')
+        const data = await res.json()
+        displayQuote(data);
+    
+    }
+    catch(error){
+        console.log(error);
+    }
 
+}
 const displayQuote = quote =>{
     const blockquote =document.getElementById('quote');
     blockquote.innerHTML = quote.quote;
