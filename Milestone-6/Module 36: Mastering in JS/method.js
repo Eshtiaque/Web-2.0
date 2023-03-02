2b4f011a03e59d06cfce7d09caf584243036ce6f
Eshtiaque <est.ahmed111@gmail.com>
2023-03-02 23:21:45 +0600

    L

--- Milestone-6/Module 36: Mastering in JS/method.js
+++ Milestone-6/Module 36: Mastering in JS/method.js
@@ -23,3 +23,43 @@ const reExam =student.improveExam('Algebra');
 // console.log(reExam);
 const remaining =student.treatDay(900);
 console.log(remaining);
+
+
+// What would be the output of?:
+
+const getGirlFriend= (name = "chokina")=>"name"; 
+console.log(getGirlFriend()); 
+
+
+let p='Javascript'; 
+let q=p; 
+p='React'; 
+console.log(q); 
+
+
+const isTrue='false'; 
+if(!isTrue){
+console.log('hello');
+} else {
+console.log('world'); 
+}
+
+
+
+function sum(p, q) {
+    p + q;
+   }
+   const result = sum(2, 3);
+   console.log(result);
+   
+
+   if ("2" === 2) {
+    console.log("Inside if");
+   } else {
+    console.log("Inside else");
+   }
+
+   function work(x, y = 4) {
+    return x + y;
+   }
+   console.log(work(32));
