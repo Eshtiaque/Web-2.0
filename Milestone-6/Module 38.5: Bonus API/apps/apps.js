b3b77b41f5e8a8e8c867c4fb5ef924ed52324395
Eshtiaque <est.ahmed111@gmail.com>
2023-03-08 18:02:20 +0600

    L

--- Milestone-6/Module 38.5: Bonus API/apps/apps.js
+++ Milestone-6/Module 38.5: Bonus API/apps/apps.js
@@ -1,6 +1,4 @@
  const API_KEY =`e60af6419dee910e665979f7472f2166`;
-
-
  const loadTemperature = city =>{
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
@@ -12,7 +10,7 @@ const displayTemperature= data =>{
     // temperature.innerText=data.main.temp;
     setInnerTextById('temperature',data.main.temp);
     setInnerTextById('condition',data.weather[0].main);
-    console.log(data.weather[0].main);
+    console.log(data);
 }
 const setInnerTextById=(id,text)=>{
     const temperature = document.getElementById(id);
@@ -24,6 +22,6 @@ document.getElementById('button-search').addEventListener('click',function(){
     //set city
 
     document.getElementById('city').innerText =city;
-    loadTemperature(city);
+    loadTemperature(city); 
 });
  loadTemperature('dhaka');
