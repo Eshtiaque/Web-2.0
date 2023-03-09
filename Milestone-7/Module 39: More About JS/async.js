293088cb0a3b0ccfb77cc8f9b33149400d0550b1
Eshtiaque <est.ahmed111@gmail.com>
2023-03-09 18:07:59 +0600

    L

---
+++ Milestone-7/Module 39: More About JS/async.js
@@ -0,0 +1,16 @@
+console.log(1);
+console.log(2);
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// setTimeout(doSomething,4000);
+setTimeout(()=>{
+    console.log('lazy-log');
+},4000);
+console.log(5);
+console.log(6);
+console.log(7);
+
+function doSomething(){
+    console.log(3);
+}
