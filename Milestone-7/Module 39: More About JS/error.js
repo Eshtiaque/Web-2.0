d496429213fba4ffd21927427afd9f9b2f1eef17
Eshtiaque <est.ahmed111@gmail.com>
2023-03-10 20:11:05 +0600

    L

---
+++ Milestone-7/Module 39: More About JS/error.js
@@ -0,0 +1,10 @@
+function checkAge(){
+   const ageField =document.getElementById('age');
+   const ageText =ageField.ariaValueMax;
+   try{
+
+   }
+   catch(err){
+    console.log('ERROR',err);
+   }
+}
