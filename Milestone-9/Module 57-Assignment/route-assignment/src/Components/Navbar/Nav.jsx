4930530ac3bb727f78c51fe2067838f6f091fed8
Eshtiaque <est.ahmed111@gmail.com>
2023-04-12 19:52:50 +0600

    l

--- Milestone-9/Module 57-Assignment/route-assignment/src/Components/Navbar/Nav.jsx
+++ Milestone-9/Module 57-Assignment/route-assignment/src/Components/Navbar/Nav.jsx
@@ -23,3 +23,11 @@ const Nav = () => {
 };
 
 export default Nav;
+
+// About the project:
+//1. Job search functionality: The application should provide job search functionality, allowing job seekers to search for jobs based on their preferred location, job title, and industry. The application should be able to display relevant job listings that match the job seeker's search criteria.
+//2. Resume/CV builder: The application should provide a resume/CV builder, allowing job seekers to create a professional-looking resume that showcases their skills, experience, and education. The application should allow job seekers to save their resume/CV for future use and allow them to download it in a variety of formats.
+//3. Personalized dashboard: The application should provide a personalized dashboard for each job seeker, allowing them to track their job applications, saved job listings, and resume/CV. The dashboard should also provide recommendations for job listings based on the job seeker's search history and preferences.
+//4. Interview preparation tools: The application should provide interview preparation tools, such as interview question generators, interview tips and advice, and mock interview simulations. These tools can help job seekers prepare for job interviews and increase their chances of getting hired.
+//5. Career advice and resources: The application should provide career advice and resources, such as job market trends, salary information, and career development tips. These resources can help job seekers make informed decisions about their career paths and improve their chances of success in the job market.
+
