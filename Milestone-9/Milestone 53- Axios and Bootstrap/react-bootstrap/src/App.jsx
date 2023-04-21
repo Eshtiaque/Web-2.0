a5e44edf6c21f53474b15dee11e665685906b7df
Eshtiaque <est.ahmed111@gmail.com>
2023-04-21 15:52:22 +0600

    L

---
+++ Milestone-9/Milestone 53- Axios and Bootstrap/react-bootstrap/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Button } from 'react-bootstrap'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* react bootstrap */}
+     <Button variant='warning '>MY React Button</Button>
+     <Button variant='danger'>MY React Button</Button>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+     <div className=''>
+     <button type="button" className="btn btn-primary">Primary</button>
+<button type="button" className="btn btn-secondary">Secondary</button>
+<button type="button" className="btn btn-success">Success</button>
+<button type="button" className="btn btn-danger">Danger</button>
+<button type="button" className="btn btn-warning">Warning</button>
+<button type="button" className="btn btn-info">Info</button>
+<button type="button" className="btn btn-light">Light</button>
+<button type="button" className="btn btn-dark">Dark</button>
+     </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ Milestone-9/Milestone 53- Axios and Bootstrap/react-bootstrap/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+//install react bootstrap with command the copy the link ->
+import 'bootstrap/dist/css/bootstrap.min.css';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+)
