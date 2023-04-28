1a1d987fdfa490fb480cd5d49a7265400bd5bfe3
Eshtiaque <est.ahmed111@gmail.com>
2023-04-28 16:11:12 +0600

    L

--- Milestone-9/Milestone 54-React-router/react-router/src/Components/SingleFriend/SingleFriend.jsx
+++ Milestone-9/Milestone 54-React-router/react-router/src/Components/SingleFriend/SingleFriend.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SingleFriend = ({ friend }) => {
     const { name, email, id, phone } = friend;
     return (
         <div className=' p-2 text-left 
- bg-black text-white border-2 border-black rounded-md m-2'>
+ bg-black text-white border-2 border-black rounded-md m-1 '>
 
             <h3>{id} : Name: {name}</h3>
             <p>email: {email}</p>
-            <p>Phone: {phone}</p>
+            <p className='mb-2'>Phone: {phone}</p>
+    <p ><Link to={`/love/${id}`} className='text-yellow-400 rounded border p-1'>"Show Me Details"</Link></p>
         </div>
     );
 };
