d74e1918afcd00e70fa93e4dd146f09f21b6ce3f
Eshtiaque <est.ahmed111@gmail.com>
2023-05-03 23:26:40 +0600

    L

---
+++ Milestone-10/Module 58-Firebase/firebase-project/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXDPip5fZK9-_YGcQJ7DB8pFD861n_qxY",
+  authDomain: "simple-firebase-c99be.firebaseapp.com",
+  projectId: "simple-firebase-c99be",
+  storageBucket: "simple-firebase-c99be.appspot.com",
+  messagingSenderId: "1029065476093",
+  appId: "1:1029065476093:web:387c27ee3f2a2665fd3b94"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ Milestone-10/Module 58-Firebase/firebase-project/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Sajid</h2>
+        </div>
+    );
+};
+
+export default Header;
