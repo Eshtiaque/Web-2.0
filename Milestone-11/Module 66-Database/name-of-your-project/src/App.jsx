94843b68069ab807373e0f167669743697443141
Eshtiaque <est.ahmed111@gmail.com>
2023-05-17 23:48:27 +0600

    L

---
+++ Milestone-11/Module 66-Database/name-of-your-project/src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+const [users,setUsers] = useState([])
+useEffect(()=>{
+  fetch('http://localhost:5000/users')
+  .then(res=>res.json())
+  .then (data=>setUsers(data))
+},[])
+
+const handleAddUser=event =>{
+  event.preventDefault();
+  const form =event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const user ={name,email};
+  console.log(user);
+  //client side sent data via post
+  fetch('http://localhost:5000/users',{
+    //set fetch method inside the fetch option.
+    //3 option
+    method: 'POST',
+    headers:{
+      'content-type':'application/json'
+    },
+    //stringify the body data
+    body:JSON.stringify(user)
+  })
+  .then(res=>res.json())
+  .then (data=>{
+    console.log('inside post response',data);
+    const newUsers =[...users,data];
+    setUsers(newUsers);
+    form.reset();
+  })
+
+}
+
+  return (
+    <>
+   
+      <h1>User Management System </h1>
+      <h3>Numbers of users : {users.length}</h3>
+      <form  onSubmit={handleAddUser}>
+        <input type="text" name='name' id='' />
+        <input type="email" name='email' id='' />
+        <input type="submit" name='Add user' id='' />
+      </form>
+    {
+users.map(user=><p key={user.id}>{user.id} : {user.name} : {user.email}</p>)
+    }
+    </>
+  )
+}
+
+export default App
