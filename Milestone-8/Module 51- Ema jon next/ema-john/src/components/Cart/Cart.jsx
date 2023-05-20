575d11452e4535c6e9e31607606ef7a99ee4da86
Eshtiaque <est.ahmed111@gmail.com>
2023-05-20 13:20:46 +0600

    update

--- Milestone-8/Module 51- Ema jon next/ema-john/src/components/Cart/Cart.jsx
+++ Milestone-8/Module 51- Ema jon next/ema-john/src/components/Cart/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = ({ cart }) => {
     for (const product of cart) {
         product.quantity =product.quantity || 1;
         totalPrice = totalPrice + product.price * product.quantity;
-        totalShipping = totalShipping + product.Shipping;
+ totalShipping = totalShipping + product.Shipping;
         quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 / 100;
